a8d1561a32c4d5a5e13f05632d2117faadcfd13f
daltonream <daltonream@gmail.com>
2020-11-16 22:39:28 -0500

    Done with step 6 wwhoop whoop

--- src/Controller/FXMLDocumentController.java
+++ src/Controller/FXMLDocumentController.java
@@ -17,6 +17,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -483,8 +484,33 @@ public class FXMLDocumentController implements Initializable {
     }
 
     @FXML
-    void ShowDetailsInPlace(ActionEvent event) {
+    void ShowDetailsInPlace(ActionEvent event) throws IOException {
+         System.out.println("clicked");
 
+        
+        Mealmodel meal = table.getSelectionModel().getSelectedItem();
+
+        
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/View/DetailedModelView.fxml"));
+
+        Parent detailedModelView = loader.load();
+
+        Scene tableViewScene = new Scene(detailedModelView);
+
+        DetailedModelViewController detailedControlled = loader.getController();
+
+
+        detailedControlled.initData(meal);
+
+        Scene currentScene = ((Node) event.getSource()).getScene();
+        detailedControlled.setPreviousScene(currentScene);
+
+        Stage stage = (Stage) currentScene.getWindow();
+
+        stage.setScene(tableViewScene);
+        stage.show();
+
+        
     }
 
  @FXML
